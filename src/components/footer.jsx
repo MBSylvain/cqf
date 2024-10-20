5e959d1ed1dfcd2f4b918f522492ea5382c841d1
MBSylvain <155894547+MBSylvain@users.noreply.github.com>
2024-10-20 14:43:30 +0200

    correction test page speedd insight

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="bg-gray-800  p-4 mt-8">
-      <div className="container mx-auto text-center">
+      <div className="container mx-auto text-center text-white">
         &copy; 2024 Photographe. Tous droits réservés.
       </div>
     </footer>
