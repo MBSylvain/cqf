9fec10420edb8639bb797d376de33c0573830d9e
MBSylvain <155894547+MBSylvain@users.noreply.github.com>
2024-10-20 23:41:13 +0200

    Update footer background color in footer.jsx

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="w-screen">
-      <div className="flex flex-row w-screen bg-gray-800 p-4">
+      <div className="flex flex-row w-screen bg-gray-900 p-4">
         <p className="w-screen text-white text-center">
           {" "}
           &copy; 2024 Photographe. Tous droits réservés.
